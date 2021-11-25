cd782fe02bf38086aa5a28cb3af843d7f913852f
Josh Cook <joshuacook0023@gmail.com>
2021-11-25 15:35:06 -0500

    feat: text summarization with GPT-3

---
+++ src/gpt3_summarization.py
@@ -0,0 +1,57 @@
+"""Summarization using GTP-3."""
+
+import os
+from typing import Any, Literal
+
+import openai
+from pydantic import BaseModel, PositiveInt
+from pydantic.types import PositiveFloat
+
+OpenaiGpt3Engine = Literal["davinci", "curie", "babbage", "ada"]
+
+
+class Gpt3SummarizationConfiguration(BaseModel):
+    """GPT-3 configuration parameters."""
+
+    engine: OpenaiGpt3Engine = "davinci"
+    temperature: PositiveFloat = 0.3
+    max_tokens: PositiveInt = 60
+    top_p: PositiveFloat = 1.0
+    frequency_penalty: float = 0.0
+    presence_penalty: float = 0.0
+
+
+def _openai_api_key() -> None:
+    if (openai_key := os.getenv("OPENAI_API_KEY")) is None:
+        raise BaseException("OpenAI API key not found.")
+    openai.api_key = openai_key
+    return None
+
+
+def _text_to_gpt3_prompt(text: str) -> str:
+    prefix = 'Summarize the following scientific article:\n"""\n'
+    suffix = '\n"""\nSummary:\n"""\n'
+    prompt = prefix + text + suffix
+    return prompt
+
+
+def _extract_gpt3_result(gpt3_response: dict) -> str:
+    return gpt3_response["choices"][0]["text"]
+
+
+def summarize(text: str, config_kwargs: dict[str, Any]) -> str:
+    """Summarize text using GPT-3.
+
+    Args:
+        text (str): Input text.
+        config_kwargs (dict[str, Any]): GPT-3 configuration parameters.
+
+    Returns:
+        str: Summarized test.
+    """
+    _openai_api_key()
+    config = Gpt3SummarizationConfiguration(**config_kwargs)
+    prompt = _text_to_gpt3_prompt(text)
+    res = openai.Completion.create(prompt=prompt, **config.dict(), stop=['"""'])
+    text_sum = _extract_gpt3_result(res)
+    return text_sum
--- summarize.py
+++ summarize.py
@@ -5,10 +5,14 @@
 from enum import Enum
 from typing import Any, Callable, Optional
 
+from dotenv import load_dotenv
 from typer import Typer
 
 from src.bart_summarization import summarize as bart_summarize
+from src.gtp3_summarization import summarize as gpt3_summarize
 from src.pagerank_summarization import summarize as pagerange_summarize
+
+load_dotenv()
 
 app = Typer()
 
@@ -26,6 +30,7 @@ summarization_callable = Callable[[str, dict[str, Any]], str]
 summarization_callables: dict[SummarizationMethod, summarization_callable] = {
     SummarizationMethod.TEXTRANK: pagerange_summarize,
     SummarizationMethod.BART: bart_summarize,
+    SummarizationMethod.GPT3: gpt3_summarize,
 }
 
 
@@ -58,48 +63,66 @@ frequencies in colorectal adenocarcinoma (COAD), lung adenocarcinoma (LUAD), mul
 myeloma (MM), and pancreatic adenocarcinoma (PAAD). Importantly, the activating alleles
 found in KRAS vary substantially across cancers, indicating possible differences in
 signaling behavior of the mutant proteins that exploit the environment of the specific
-cellular context3,4.
-When mutated at one of its four hotspot codons—12, 13, 61, or 146—activated KRAS protein
-hyperactivates many downstream effector pathways, such as the MAPK and PI3K-AKT
-signaling pathways1. Previous studies have documented substantial differences in the
-biochemical and signaling properties of the common KRAS variants (reviewed by Miller et
-al.5 and Li et al.6). KRAS normally operates as a molecular switch, activating
-downstream pathways when GTP-bound, but inactive when GDP-bound following the hydrolysis
-of the 𝛾
-γ-phosphate. This reaction is catalyzed by GTPase-activating proteins (GAPs), while the
-exchange of the GDP for a new GTP is facilitated by guanine nucleotide exchange factors
-(GEFs)7. Activating KRAS mutations result in elevated engagement of downstream pathways
-by increasing the steady-state levels of GTP-bound KRAS. Specifically, mutations to
-codons 12, 13, and 61 reduce the rate of intrinsic and/or GAP-mediated hydrolysis, and
-mutations at 13 and 61, but not 12, also enhance the rate of nucleotide exchange8,9.
-Alternatively, 146 mutations do not alter the rate of GTP hydrolysis, but cause
-hyperactivation through an increased rate of GDP exchange4,10,11,12. Additional
-biochemical, structural, and signaling distinctions have been identified between
-different mutant alleles, including between those at the same amino acid position4,8,13,
-14,15,16,17,18,19,20.
-Likely as a consequence of their distinct properties, associations have been uncovered
-between the specific KRAS mutation status and therapeutic responses and clinical
-outcomes of cancer patients3,6. For instance, a retrospective meta-analysis suggested
-that COAD tumors with a KRAS G13D allele were sensitive to anti-EGFR therapies, a
-treatment generally discouraged for KRAS-mutant tumors21. It has recently been proposed,
-via computational and experimental means, that differential interaction kinetics between
-KRAS G13D and the Ras GAP NF-1 explain this effect22,23,24. Another example is that the
-KRAS G12D allele is associated with worse overall survival in advanced PAAD, when
-compared to patients with WT KRAS, KRAS G12R, or KRAS G12V25. Thus far, the hypothesis
-has been that the different biological properties of the mutant KRAS alleles are the
-cause of these clinical distinctions. However, it is also possible that allele-specific
-genetic interactions drive the varying clinical outcomes.
+cellular context3,4. When mutated at one of its four hotspot codons—12, 13, 61, or
+146—activated KRAS protein hyperactivates many downstream effector pathways, such as the
+MAPK and PI3K-AKT signaling pathways1. Previous studies have documented substantial
+differences in the biochemical and signaling properties of the common KRAS variants
+(reviewed by Miller et al.5 and Li et al.6). KRAS normally operates as a molecular
+switch, activating downstream pathways when GTP-bound, but inactive when GDP-bound
+following the hydrolysis of the 𝛾-phosphate. This reaction is catalyzed by GTPase-
+activating proteins (GAPs), while the exchange of the GDP for a new GTP is facilitated
+by guanine nucleotide exchange factors (GEFs)7. Activating KRAS mutations result in
+elevated engagement of downstream pathways by increasing the steady-state levels of
+GTP-bound KRAS. Specifically, mutations to codons 12, 13, and 61 reduce the rate of
+intrinsic and/or GAP-mediated hydrolysis, and mutations at 13 and 61, but not 12, also
+enhance the rate of nucleotide exchange8,9. Alternatively, 146 mutations do not alter
+the rate of GTP hydrolysis, but cause hyperactivation through an increased rate of GDP
+exchange4,10,11,12. Additional biochemical, structural, and signaling distinctions have
+been identified between different mutant alleles, including between those at the same
+amino acid position4,8,13,14,15,16,17,18,19,20. Likely as a consequence of their
+distinct properties, associations have been uncovered between the specific KRAS mutation
+status and therapeutic responses and clinical outcomes of cancer patients3,6. For
+instance, a retrospective meta-analysis suggested that COAD tumors with a KRAS G13D
+allele were sensitive to anti-EGFR therapies, a treatment generally discouraged for
+KRAS-mutant tumors21. It has recently been proposed, via computational and experimental
+means, that differential interaction kinetics between KRAS G13D and the Ras GAP NF-1
+explain this effect22,23,24. Another example is that the KRAS G12D allele is associated
+with worse overall survival in advanced PAAD, when compared to patients with WT KRAS,
+KRAS G12R, or KRAS G12V25. Thus far, the hypothesis has been that the different
+biological properties of the mutant KRAS alleles are the cause of these clinical
+distinctions. However, it is also possible that allele-specific genetic interactions
+drive the varying clinical outcomes. Understanding the heterogeneous properties of the
+KRAS alleles is essential to effectively treating KRAS-driven cancers. Here, we study
+the origins of KRAS mutations to assess the extent to which tissue-specific mutational
+processes determined the allelic distribution. We then construct comutation networks for
+each KRAS allele to identify different properties of the alleles. Finally, we analyze
+allele-specific genetic dependencies to explore potential therapeutic targets. Our
+analysis demonstrates that an allele-specific and tissue-specific analysis is necessary
+to fully understand the nature of the most potent oncogenes.
 """
+
+
+def _pre_summary_message(method: SummarizationMethod) -> None:
+    method_msg = f"Summarization method: '{method.value}'"
+    print(method_msg)
+    print("-" * len(method_msg))
+    return None
+
+
+def _get_test_text() -> str:
+    full_text = ""
+    for line in test_text.splitlines():
+        full_text += " " + line.strip()
+    return full_text
 
 
 @app.command()
 def main() -> None:
     """Run the summarization methods."""
+    text = _get_test_text()
     for method in SummarizationMethod:
-        method_msg = f"Summarization method: '{method.value}'"
-        print(method_msg)
-        print("-" * len(method_msg))
-        res = summarize(method=method, text=test_text)
+        _pre_summary_message(method)
+        res = summarize(method=method, text=text)
         print(res)
         print("-" * 80 + "\n")
     return None
