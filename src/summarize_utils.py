28b67791e405187ed4307091906abccd33f2f844
Josh Cook <joshuacook0023@gmail.com>
2021-11-30 08:22:28 -0500

    refactor: move logic out of CLI entrypoint and into a module in src

---
+++ src/summarize_utils.py
@@ -0,0 +1,182 @@
+"""Utilities for the main summarization script."""
+
+
+from enum import Enum
+from typing import Any, Callable, Final, Optional, Union
+
+from pydantic.main import BaseModel
+
+from src.bart_summarization import BartSummarizationConfiguration
+from src.bart_summarization import summarize as bart_summarize
+from src.gpt3_summarization import Gpt3SummarizationConfiguration
+from src.gpt3_summarization import summarize as gpt3_summarize
+
+# from src.pagerank_summarization import PageRankSummarizationConfiguration
+from src.pagerank_summarization import summarize as pagerange_summarize
+from src.parse_scientific_article import parsed_article
+
+
+class SummarizationMethod(Enum):
+    """Available summarization method."""
+
+    TEXTRANK = "TEXTRANK"
+    BART = "BART"
+    GPT3 = "GPT3"
+
+
+def _pre_summary_message(method: SummarizationMethod) -> None:
+    method_msg = f"Summarization method: '{method.value}'"
+    print(method_msg)
+    print("=" * len(method_msg))
+    return None
+
+
+def _pre_section_message(name: str) -> None:
+    print("\n" + name)
+    print("-" * len(name))
+    return None
+
+
+def _word_count(x: str) -> int:
+    return len(x.split(" "))
+
+
+# TODO: change the following function so that it augments a configuration instead of
+# returning a novel one.
+
+
+def _get_best_configuration_kwargs(
+    method: SummarizationMethod, text: str
+) -> dict[str, Any]:
+    n_words = _word_count(text)
+    if method is SummarizationMethod.BART:
+        config = BartSummarizationConfiguration(
+            max_length=int(n_words * 0.2), min_length=int(n_words * 0.05)
+        )
+        return config.dict()
+    elif method is SummarizationMethod.GPT3:
+        config = Gpt3SummarizationConfiguration(max_tokens=(n_words * 0.15))
+        return config.dict()
+    else:
+        return {}
+
+
+summarization_callable = Callable[[str, dict[str, Any]], str]
+
+SUMMARIZATION_CALLABLES: dict[SummarizationMethod, summarization_callable] = {
+    SummarizationMethod.TEXTRANK: pagerange_summarize,
+    SummarizationMethod.BART: bart_summarize,
+    SummarizationMethod.GPT3: gpt3_summarize,
+}
+
+
+def _summarize(
+    method: SummarizationMethod, text: str, kwargs: Optional[dict[str, Any]] = None
+) -> str:
+    fxn = SUMMARIZATION_CALLABLES.get(method)
+    if kwargs is None:
+        kwargs = {}
+    if fxn is None:
+        raise NotImplementedError(method.value)
+    return fxn(text, kwargs)
+
+
+KEEP_SECTIONS = ["Introduction", "Results", "Discussion", "Results and discussion"]
+
+
+def _preprocess_article(article: parsed_article, max_len: int = -1) -> parsed_article:
+    # Filter for only certain sections.
+    new_article = {k: t for k, t in article.items() if k in KEEP_SECTIONS}
+    # Merge shorter paragraphs.
+    for title, paragraphs in new_article.items():
+        merged_p = ""
+        new_ps: list[str] = []
+        for p in paragraphs:
+            if max_len < 0 or _word_count(merged_p + " " + p) < max_len:
+                merged_p += " " + p
+            else:
+                new_ps.append(merged_p)
+                merged_p = p
+        if len(merged_p) > 0:
+            new_ps.append(merged_p)
+        new_article[title] = new_ps
+    return new_article
+
+
+summarization_method_max_lengths: Final[dict[SummarizationMethod, int]] = {
+    SummarizationMethod.BART: 650,
+    SummarizationMethod.GPT3: 1200,
+}
+
+
+def get_urls() -> set[str]:
+    """Get the URLs for the articles to summarize.
+
+    Returns:
+        set[str]: Set of URLs.
+    """
+    return {
+        "https://www.nature.com/articles/s41467-021-22125-z",
+    }
+
+
+class SummarizationConfiguration(BaseModel):
+    """Configuration for a summarization."""
+
+    method: SummarizationMethod
+    config_kwargs: Optional[dict[str, Union[float, str, bool]]] = None
+
+
+def generate_configurations() -> list[SummarizationConfiguration]:
+    """Get configurations to use for the summarizations.
+
+    Returns:
+        list[SummarizationConfiguration]: List of summarization configurations.
+    """
+    textrank_ratios = [0.01, 0.05, 0.1, 0.2]
+    return [
+        SummarizationConfiguration(
+            method=SummarizationMethod.TEXTRANK, config_kwarg={"ratio": ratio}
+        )
+        for ratio in textrank_ratios
+    ]
+
+
+class SummarizedArticle(BaseModel):
+    """The results of summarizing an article."""
+
+    url: str
+    config: SummarizationConfiguration
+    summary: parsed_article
+
+
+def summarize_article(
+    url: str, article: parsed_article, config: SummarizationConfiguration
+) -> SummarizedArticle:
+    """Summarized an article.
+
+    Args:
+        url (str): URL for the article's webpage.
+        article (parsed_article): The parsed article.
+        config (SummarizationConfiguration): A configuration for the summarization
+        method.
+
+    Returns:
+        SummarizedArticle: The summarized article.
+    """
+    method = config.method
+    max_len = summarization_method_max_lengths.get(config.method, -1)
+    article = _preprocess_article(article.copy(), max_len=max_len)
+    _pre_summary_message(method)
+    summary_dict: parsed_article = {}
+    for title, paragraphs in article.items():
+        _pre_section_message(title)
+        summarized_paragraphs: list[str] = []
+        for paragraph in paragraphs:
+            config_kwargs = _get_best_configuration_kwargs(method, paragraph)
+            res = _summarize(method=method, text=paragraph, kwargs=config_kwargs)
+            res = res.strip()
+            if len(res) > 0:
+                summarized_paragraphs.append(res)
+        summary_dict[title] = summarized_paragraphs
+    return SummarizedArticle(url=url, config=config, summary=summary_dict)
--- summarize.py
+++ summarize.py
@@ -2,155 +2,49 @@
 
 """Entrypoint to summarization functions."""
 
-from enum import Enum
-from typing import Any, Callable, Final, Optional
-
 from dotenv import load_dotenv
 from typer import Typer
 
-from src.bart_summarization import BartSummarizationConfiguration
-from src.bart_summarization import summarize as bart_summarize
-from src.gpt3_summarization import Gpt3SummarizationConfiguration
-from src.gpt3_summarization import summarize as gpt3_summarize
-from src.pagerank_summarization import summarize as pagerange_summarize
-from src.parse_scientific_article import get_and_parse_article, parsed_article
-
-URL: Final[str] = "https://www.nature.com/articles/s41467-021-22125-z"
+from src.parse_scientific_article import get_and_parse_article
+from src.summarize_utils import (
+    SummarizedArticle,
+    generate_configurations,
+    get_urls,
+    summarize_article,
+)
 
 load_dotenv()
 
 app = Typer()
 
 
-class SummarizationMethod(Enum):
-    """Available summarization method."""
+@app.command()
+def summarize() -> None:
+    """Run the summarization pipeline to summarize a series of articles.
 
-    TEXTRANK = "TEXTRANK"
-    BART = "BART"
-    GPT3 = "GPT3"
-
-
-summarization_callable = Callable[[str, dict[str, Any]], str]
-
-summarization_callables: dict[SummarizationMethod, summarization_callable] = {
-    SummarizationMethod.TEXTRANK: pagerange_summarize,
-    SummarizationMethod.BART: bart_summarize,
-    SummarizationMethod.GPT3: gpt3_summarize,
-}
-
-
-def _pre_summary_message(method: SummarizationMethod) -> None:
-    method_msg = f"Summarization method: '{method.value}'"
-    print(method_msg)
-    print("=" * len(method_msg))
-    return None
-
-
-def _pre_section_message(name: str) -> None:
-    print("\n" + name)
-    print("-" * len(name))
+    Run the summarization pipeline to summarize a series of articles using different
+    methods and configurations.
+    """
+    articles = {url: get_and_parse_article(url) for url in get_urls()}
+    summarized_articles: list[SummarizedArticle] = []
+    for summ_config in generate_configurations():
+        for url, article in articles.items():
+            summarized_article = summarize_article(url, article, config=summ_config)
+            summarized_articles.append(summarized_article)
     return None
 
 
 @app.command()
-def parse_article(url: str = URL) -> dict[str, list[str]]:
-    """CLI to parse an article's webpage.
+def parse_article(url: str) -> dict[str, list[str]]:
+    """CLI entrypoint to parse an article's webpage.
 
     Args:
-        url (str, optional): URL to an article's webpage. Defaults to URL.
+        url (str): URL to an article's webpage.
 
     Returns:
         dict[str, list[str]]: Parsed article.
     """
     return get_and_parse_article(url=url)
-
-
-def _word_count(x: str) -> int:
-    return len(x.split(" "))
-
-
-def _get_best_configuration_kwargs(
-    method: SummarizationMethod, text: str
-) -> dict[str, Any]:
-    n_words = _word_count(text)
-    if method is SummarizationMethod.BART:
-        config = BartSummarizationConfiguration(
-            max_length=int(n_words * 0.2), min_length=int(n_words * 0.05)
-        )
-        return config.dict()
-    elif method is SummarizationMethod.GPT3:
-        config = Gpt3SummarizationConfiguration(max_tokens=(n_words * 0.15))
-        return config.dict()
-    else:
-        return {}
-
-
-def _summarize(
-    method: SummarizationMethod, text: str, kwargs: Optional[dict[str, Any]] = None
-) -> str:
-    fxn = summarization_callables.get(method)
-    if kwargs is None:
-        kwargs = {}
-    if fxn is None:
-        raise NotImplementedError(method.value)
-    return fxn(text, kwargs)
-
-
-KEEP_SECTIONS = ["Introduction", "Results", "Discussion", "Results and discussion"]
-
-
-def _preprocess_article(article: parsed_article, max_len: int = -1) -> parsed_article:
-    # Filter for only certain sections.
-    new_article = {k: t for k, t in article.items() if k in KEEP_SECTIONS}
-    # Merge shorter paragraphs.
-    for title, paragraphs in new_article.items():
-        merged_p = ""
-        new_ps: list[str] = []
-        for p in paragraphs:
-            if max_len < 0 or _word_count(merged_p + " " + p) < max_len:
-                merged_p += " " + p
-            else:
-                new_ps.append(merged_p)
-                merged_p = p
-        if len(merged_p) > 0:
-            new_ps.append(merged_p)
-        new_article[title] = new_ps
-    return new_article
-
-
-summarization_method_max_lengths: Final[dict[SummarizationMethod, int]] = {
-    SummarizationMethod.BART: 650,
-    SummarizationMethod.GPT3: 1200,
-}
-
-
-def _print_article(article: parsed_article) -> None:
-    for title, text in article.items():
-        print(f"{title}: {len(text)} ({[_word_count(t) for t in text]})")
-
-
-@app.command()
-def summarize(url: str) -> None:
-    """Summarize an article from its webpage.
-
-    Args:
-        url (str): URL to the article's webpage.
-    """
-    for method in SummarizationMethod:  # [SummarizationMethod.GPT3]:
-        max_len = summarization_method_max_lengths.get(method, -1)
-        article = _preprocess_article(parse_article(url), max_len=max_len)
-        _print_article(article)
-        _pre_summary_message(method)
-        for title, paragraphs in article.items():
-            _pre_section_message(title)
-            for paragraph in paragraphs:
-                config_kwargs = _get_best_configuration_kwargs(method, paragraph)
-                res = _summarize(method=method, text=paragraph, kwargs=config_kwargs)
-                res = res.strip()
-                if len(res) > 0:
-                    print(res)
-        print("-" * 80 + "\n")
-    return None
 
 
 if __name__ == "__main__":
